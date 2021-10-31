8a02369ce0cccd9c3603477e55446c2ddb174736
4allofus <33950526+4allofus@users.noreply.github.com>
2021-10-31 23:24:15 +0700

    add targetlocation hope works

--- src/routes/paperkit.svelte
+++ src/routes/paperkit.svelte
@@ -16,6 +16,8 @@
 	// since there's no dynamic data here, we can prerender
 	// it so that it gets served as a static asset in prod
 	export const prerender = true;
+
+	let targetLocation = 'jakarta';
 </script>
 
 <svelte:head>
@@ -24,7 +26,13 @@
 
 <div class="container-md">
 	<h1>SPaper</h1>
-	<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=University of Oxford&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.fnfgo.com/">FNF</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
+	<div class="mapouter">
+	<div class="gmap_canvas">
+	<iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+	src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;
+	q={targetLocation}}&amp;
+	t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+	</iframe><a href="https://www.fnfgo.com/">FNF</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
 
 </div>
 
