a2ee1969e1a3b6fa2eca39f526487f7a5a365d4d
4allofus <33950526+4allofus@users.noreply.github.com>
2021-10-31 23:02:33 +0700

    header nav fix 3: nav instead of navbar

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -7,19 +7,30 @@
 </script>
 
 <header>
-
-	<Navbar border={false}>
-    	<h3 slot="PaperKit">
-      	<a href="#">Paperkit</a>
-    	</h3>
-    	<ul class="inline">
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+<nav class="border fixed split-nav">
+  <div class="nav-brand">
+    <h3><a href="#">Listing bank</a></h3>
+  </div>
+  <div class="collapsible">
+    <input id="collapsible1" type="checkbox" name="collapsible1">
+    <label for="collapsible1">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+	  <div class="bar4"></div>
+    </label>
+    <div class="collapsible-body">
+      <ul class="inline">
+        <li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
-			<li class:active={$page.path === '/paperkit'}><a sveltekit:prefetch href="/paperkit">paperkit</a></li>		
-    	</ul>
-  	</Navbar>
+			<li class:active={$page.path === '/paperkit'}><a sveltekit:prefetch href="/paperkit">paperkit</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
+	
 </header>
 
 <style>
